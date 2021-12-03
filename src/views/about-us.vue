407940092ff2e14ea2ca24f7d3024f5ae07786ce
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2021-12-03 23:16:07 +0100

    Done with the about page

--- src/views/about-us.vue
+++ src/views/about-us.vue
@@ -4,18 +4,58 @@
     <h1>About us</h1>
   </div>
 </section>
+
+<section class="who-we-are">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6 item">
+        <div class="content">
+          <h2>Who we are</h2>
+          <p>Established in 1990, G&T Construction Ltd has evolved from a combination of its electrical, engineering and design consultancy roots to a main contracting business with capability in delivering a wide range of construction projects across the Southwest of England. We have strong roots from our origin in electrical and specialist engineering combined with experience of providing design and construction management consultancy in the UK for over 30 years.</p>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 item"></div>
+    </div>
+  </div>
+</section>
+<section class="images-view">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6 item">
+        <div class="img-view-1"></div>
+      </div>
+      <div class="col-12 col-md-6 item">
+        <div class="img-view-2"></div>
+        <div class="img-view-3"></div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<Footer />
 </template>
 
 <script setup>
-
+import Footer from '../components/common/Footer.vue';
+title: 'About us | G&T'
 </script>
 
 <style lang="scss" scoped>
 .bg-hero {
-  background: linear-gradient(rgba(0, 0, 0, 0.48), rgba(0, 0, 0, 0.48)), url('../../public/img/pexels-mikael-blomkvist-8961401\ 1.png') !important;
+  background: linear-gradient(rgba(0, 0, 0, 0.48), rgba(0, 0, 0, 0.48)), url('/img/pexels-mikael-blomkvist-8961401\ 1.png') !important;
   background-repeat: no-repeat !important;
   background-size: cover !important;
   background-position: center !important;
   height: 80vh;
+  display: flex;
+  align-items: center;
+  .content {
+    width: 400px;
+    margin: auto;
+    h1 {
+      font-size: 80px;
+    font-weight: bold;
+    }
+  }
 }
 </style>
